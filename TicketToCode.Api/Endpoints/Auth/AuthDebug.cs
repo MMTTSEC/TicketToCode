8896c4acf3c27d6a6c418cf08ef94f274c6ce0f1
mycookie5 <fanual.abrahm@gmail.com>
2025-03-19 11:06:40 +0100

    connected blazer to api and created an endpoint for debugging login

---
+++ TicketToCode.Api/Endpoints/Auth/AuthDebug.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Metadata;
+using System.Linq;
+using System.Collections.Generic;
+using TicketToCode.Api.Services;
+
+namespace TicketToCode.Api.Endpoints.Auth
+{
+    public class AuthDebug : IEndpoint
+    {
+        //Mapping
+        public static void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app
+            .MapGet("/auth/Debug", Handle)
+            .WithSummary("View user Database");
+        }
+
+    public record request();
+    public record Results(
+        int Id,
+        string Username, 
+        string PasswordHash,
+        string Role
+        );
+
+    private static List<Results> Handle(IDatabase db)
+    {
+        return db.Users
+            .Select(u => new Results(
+                u.Id,
+                u.Username,
+                u.PasswordHash,
+                u.Role
+                )).ToList();
+    }
+    }
+}
