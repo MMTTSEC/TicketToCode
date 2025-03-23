f6e4e4bab5fd87f0713bca49186581ffe2d378f7
mycookie5 <fanual.abrahm@gmail.com>
2025-03-23 12:12:53 +0100

    Add service, method and endpoint for sort

---
+++ TicketToCode.Api/Endpoints/Sort/SortByStartTime.cs
@@ -0,0 +1,37 @@
+﻿using TicketToCode.Core.Interface;
+
+namespace TicketToCode.Api.Endpoints.Sort
+{
+    public class SortByStartTime : IEndpoint
+    {
+        public static void MapEndpoint(IEndpointRouteBuilder app) => app
+            .MapGet("/sort/starttime", Handle)
+            .WithSummary("Sort by Start Time");
+        public record Response(
+            int Id, 
+            string Name, 
+            string Description,
+            EventType Type,
+            DateTime StartTime,
+            DateTime EndTime,
+            int MaxAttendance
+            );
+
+        private static IResult Handle (ISort SortService)
+        {
+            var SortedByStartTime = SortService.SortByStartTime();
+
+            var response = SortedByStartTime.Select(x => new Response(
+                x.Id,
+                x.Name,
+                x.Description,
+                x.Type,
+                x.StartTime,
+                x.EndTime,
+                x.MaxAttendees
+
+                ));
+            return TypedResults.Ok( response );
+        }
+    }
+}
