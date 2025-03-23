c51af268b32efd60b06b2edb52932c674b292006
mycookie5 <fanual.abrahm@gmail.com>
2025-03-23 13:06:35 +0100

    Sort method and endpoint by remaining tickets

--- TicketToCode.Core/Services/SortService.cs
+++ TicketToCode.Core/Services/SortService.cs
@@ -15,5 +15,22 @@ namespace TicketToCode.Core.Services
         {
             return _database.Events.OrderBy(x => x.StartTime).ToList();
         }
+        public List<Event> SortByRemaniningTickets()
+        {
+            var eventsWithRemainingTickets = _database.Events.Select(e => new
+            {
+                Event = e,
+                // compare event id and ticket to calculate how many tickets are left.
+                RemainingTickets = e.MaxAttendees - _database.Tickets.Count(ticket => ticket.EventID == e.Id)
+            }).ToList();
+
+            var sortedEvents = eventsWithRemainingTickets
+                //sort them to be presented.
+               .OrderByDescending(x => x.RemainingTickets)
+               .Select(x => x.Event)
+               .ToList();
+
+            return sortedEvents;
+        }
     }
 }
