e81fabb49765b58166c4941271ce1c6dc6350076
mycookie5 <fanual.abrahm@gmail.com>
2025-03-23 15:13:00 +0100

    search bar endpoint starts search with name and ends with eventtype

---
+++ TicketToCode.Api/Endpoints/Filter/SearchBar.cs
@@ -0,0 +1,41 @@
+﻿using TicketToCode.Api.Endpoints;
+using TicketToCode.Core.Interface;
+
+namespace TicketTCode.Api.Endpoints.Filter
+{
+    public class SearchBar : IEndpoint
+    {
+        public static void MapEndpoint(IEndpointRouteBuilder app) => app
+            .MapGet("/filter", Handle)
+            .WithSummary("Search bar result");
+
+        public record Request(string query);
+
+        public record Response(
+            int Id,
+            string Name,
+            string Description,
+            EventType Type,
+            DateTime StartTime,
+            DateTime EndTime,
+            int MaxAttendance
+            );
+
+        private static IResult Handle([AsParameters] Request request, IFilter FilterService)
+        {
+            var searchResult = FilterService.SearchBar(request.query);
+
+            var response = searchResult.Select(x => new Response(
+                x.Id,
+                x.Name,
+                x.Description,
+                x.Type,
+                x.StartTime,
+                x.EndTime,
+                x.MaxAttendees
+
+                ));
+            return TypedResults.Ok(response);
+        }
+    }
+}
---
+++ TicketToCode.Core/Services/FilterService.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using TicketToCode.Core.Data;
+using TicketToCode.Core.Interface;
+using TicketToCode.Core.Models;
+
+namespace TicketToCode.Core.Services
+{
+    public class FilterService : IFilter
+    {
+        private readonly IDatabase _database;
+        public FilterService(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public List<Event> SearchBar(string input)
+        {
+            var matchedByName = new List<Event>();
+            var hold = new List<Event>();
+            var matchedByEventType = new List<Event>();
+
+            foreach (var e in _database.Events)
+            {
+                if (e.Name.Contains(input, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchedByName.Add(e);
+                }
+                else
+                {
+                    hold.Add(e);
+                }
+            }
+            foreach (var e in hold)
+            {
+                var matchingType = Enum.GetValues(typeof(EventType))
+                               .Cast<EventType>()
+                               .Where(et => et.ToString().Contains(input, StringComparison.OrdinalIgnoreCase))
+                               .FirstOrDefault();
+                if (matchingType != 0)
+                {
+                    if (e.Type == matchingType)
+                    {
+                        matchedByEventType.Add(e);
+                    }
+                }
+
+            }
+
+            var finalResults = matchedByName
+                .Concat(matchedByEventType)
+                .ToList();
+
+            return finalResults;
+        }
+    }
+}
